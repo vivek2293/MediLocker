077331d93d55d51fbb519c84ddddc133c6e31cd5
mandyval17 <mayurvalecha2603@gmail.com>
2022-09-10 11:52:01 +0530

    Added Server and mail service

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+// middleware
+
+app.use(cors())
+// app.use(express.static('./public'));
+app.use(express.json());
+
+// routes
+
+app.use('/', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
