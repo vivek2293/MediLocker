767702d367c1cb08104bab9cdb3cc408140d2d3e
vivek2293 <vivek22935@gmail.com>
2022-09-10 08:28:33 +0530

    Intiating project

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { useCallback } from 'react';
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import './App.css';
+import particlesOptions from "./particles.json";
+
+function App() {
+    const particlesInit = useCallback(main => {
+        loadFull(main);
+    }, [])
+
+    return (
+        <div className="App">
+            <Particles options={particlesOptions} init={particlesInit}/>
+        </div>
+    );
+}
+
+export default App;
