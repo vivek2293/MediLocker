a0486320eef9ab43827414f05f1f38db5b73410d
Nachiket <nachiketdodia33@gmail.com>
2022-09-10 17:19:06 +0530

    minor changes

--- frontend/src/components/LoginOption.jsx
+++ frontend/src/components/LoginOption.jsx
@@ -11,33 +11,32 @@ function LoginOption(props) {
   console.log(address);
   return (
     <>
-      <section className="loginOption d-flex"  id = "login-option" style={sectionStyle}>
+      <section className="loginOption "  id = "login-option" style={sectionStyle}>
         <div
-          className="container d-flex justify-content-center "
+          className="container d-flex justify-content-center align-items-center"
           style={{ height: "100vh" }} id="option-box"
         >
-          <div className="card py-5 px-5 mt-5 justify-content-around" id="optcard">
-            <h3 id = "whomsg" className="text-center display-6">
-              {" "}
+          <div className="card py-3 px-4 mt-4" id="optcard" style={{height:"39%"}}>
+            <h3 id = "whomsg" className="text-center display-md-6">
               Who Do You Want To {props.message} As?
             </h3>
             <hr id="card-line"/>
             <div className="row">
-            <div className="col-md-6 btnLayout d-flex p-3 justify-content-center align-items-center">
+            <div className="col-md-6 btnLayout d-flex p-1 justify-content-center align-items-center">
               <Link to={patientURL}>
                 <button
                   type="button"
-                  className="mx-5 rounded-pill display-4 text-center" id ="option-btn"
+                  className="mx-5 rounded-pill display-5 text-center" id ="option-btn"
                 >
                   Patient
-                </button>g
+                </button>
               </Link>
               </div>
-              <div className="col-md-6 btnLayout d-flex p-3 justify-content-center align-items-center">
+              <div className="col-md-6 btnLayout d-flex p-1 justify-content-center align-items-center">
               <Link to={doctorURL}>
                 <button
                   type="button"
-                  className="mx-5 rounded-pill display-4 text-center" id ="option-btn"
+                  className="mx-5 rounded-pill display-5 text-center" id ="option-btn"
                 >
                   Doctor
                 </button>
