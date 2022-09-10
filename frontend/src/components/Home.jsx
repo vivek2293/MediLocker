553cd3d4cd42de604f49e973be34ec54462d7af2
Nachiket <nachiketdodia33@gmail.com>
2022-09-10 16:59:34 +0530

    fixed navbar and added home screen

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import "../css/home.css";
+import HomeScreen1 from "./HomeScreen1";
 import ParticlesBg from "./ParticlesBg";
 function Home() {
   return (
     <>
       <div className="snapContainer">
         <section className="snapSection ">
-            <ParticlesBg className="particleScreen"/>
-            <div className="coverScreen">One</div>
+          <ParticlesBg className="particleScreen" />
+          <div className="coverScreen d-flex justify-content-center align-items-center">
+            <HomeScreen1 />
+          </div>
         </section>
         <section className="snapSection">
-        <div className="coverScreen">Two</div>
+          <div className="coverScreen">Two</div>
         </section>
         <section className="snapSection">
-        <div className="coverScreen">Three</div>
+          <div className="coverScreen">Three</div>
         </section>
       </div>
     </>
