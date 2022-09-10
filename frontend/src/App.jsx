a2db2bafd2b4c1df51666dad0e91e65daa05cb13
Nachiket <nachiketdodia33@gmail.com>
2022-09-10 12:31:52 +0530

    added register route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import LoginOption from "./components/LoginOption";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<LoginOption message="Login" />} />
         <Route exact path="/register" element={<LoginOption message="Register" />} />
+        <Route exact path="/register/patient" element={<Register />} />
       </Routes>
     </>
   );
