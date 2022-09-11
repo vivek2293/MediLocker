23c2a8f9c9bbbb47613aa27700ff1b5de9c1238f
Nachiket <nachiketdodia33@gmail.com>
2022-09-11 06:57:52 +0530

    fixed profile circle

--- frontend/src/components/PatientDashboard.jsx
+++ frontend/src/components/PatientDashboard.jsx
@@ -6,8 +6,17 @@ function PatientDashboard() {
     <>
       <section className="patientDashboardSection">
         <div className="row" id="userDisplay">
-          <div className="col-md-2">
+          <div className="col-md-2 d-flex justify-content-center align-items-center">
+            <svg height='150' width='150'>
+              <circle
+                cx="70"
+                cy="70"
+                r="55"
+                fill="grey"
+              />
+            </svg>
           </div>
+          <div className="col-md-10"></div>
         </div>
         <div className="row" id="renderingWindow"></div>
       </section>
