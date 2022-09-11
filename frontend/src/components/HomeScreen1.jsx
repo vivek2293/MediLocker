7a872e7204f8edb002d6f1aee7e7c700c0ac4fb3
Nachiket <nachiketdodia33@gmail.com>
2022-09-11 19:09:05 +0530

    fixed home page content

--- frontend/src/components/HomeScreen1.jsx
+++ frontend/src/components/HomeScreen1.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Background } from "tsparticles-engine";
+const headStyle = {
+  fontFamily: "Serif",
+};
+const paraStyle = {
+  fontSize: "1.5rem",
+};
 
 function HomeScreen1() {
   return (
     <>
-        <div className="container text-center d-flex flex-column align-items-center">
-            <h3 className='display-3 mt-md-4 mb-md-2'>Hello Girls</h3>
-            <p className='w-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut adipisci dolor, ad consequatur praesentium alias dolore tempora minus! Explicabo adipisci ratione voluptatum quam distinctio laborum voluptates quod saepe odit officiis!
-            Fugiat ut nostrum officiis soluta deserunt, distinctio tempor
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut adipisci dolor, ad consequatur praesentium alias dolore tempora minus!</p>
-            <button className='btn rounded-pill p-md-2 m-md-3 btn-outline-primary fw-md-bold col-md-2'>FUCK ME</button>
-        </div>
+      <div className="container text-center d-flex flex-column align-items-center">
+        <h3
+          className="display-3 mt-md-4 mb-md-2 bg-danger py-2 px-4"
+          style={headStyle}
+        >
+          MedILocker
+        </h3>
+        <p className="w-75 p-3" style={(headStyle, paraStyle)}>
+          Medical History On Finger Tips
+        </p>
+        <p className="d-none">Hello Girls</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default HomeScreen1
+export default HomeScreen1;
