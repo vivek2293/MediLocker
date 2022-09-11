55dbe536449957c24f3bebf3bbaeb6a1d1804d3e
mandyval17 <mayurvalecha2603@gmail.com>
2022-09-11 15:32:50 +0530

    otp

--- nodemailer.js
+++ nodemailer.js
@@ -40,7 +40,7 @@ const SendOTP = asyncWrapper(async (req, res, next) => {
     from: "medicare4476@outlook.com",
     to: `${req.body.email}`,
     subject: "OTP FOR VERIFICATION",
-    text: `lavde ka vinay ${app}`,
+    text: `${app}`,
   };
 
   const hashedOTP = async (otphash) => {
